39400c7382d5bec9bccb1b1edca534516268433f
David Schneck <david.schneckster@hotmail.com>
2021-04-10 23:48:48 -0400

    got some of the status component functionality working

--- src/Status.js
+++ src/Status.js
@@ -1,15 +1,19 @@
 import './styles/Status.css';
-import {useDispatch} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import {select, reset_board} from './actions/board/boardSlice.js';
 import {enqueue, dequeue} from './actions/lastCalled/lastCalledSlice.js';
 
 function Status(props) {
 	const dispatch = useDispatch();
+	const called = useSelector(state => state.lastCalled);
 	let lastCalled = 1;
 	return (
 
 		<div id="status">
-				<h1 id="called-msg">Number {lastCalled} called - found on x cards</h1>
+				{(() => { 
+					if (called.lastCalled[4] === null) return <h1 id="called-msg">Have Fun!</h1>
+					else return <h1 id="called-msg">Number {called.lastCalled[4]} called - found on x cards</h1>
+				})()}
 				<h1 className="away">1 AWAY: {0}</h1>
 				<h1 className="away">2 AWAY: {0}</h1>
 				<h1 className="away">3 AWAY: {0}</h1>
